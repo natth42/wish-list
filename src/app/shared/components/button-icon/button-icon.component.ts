1e1ea8a4b9654fc17e5d2170ed796d4dfeaea206
Nathalia <mesquita.natth@gmail.com>
2024-04-09 07:14:07 -0300

    added icons and change icon-button

--- src/app/shared/components/button-icon/button-icon.component.ts
+++ src/app/shared/components/button-icon/button-icon.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
 
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroMagnifyingGlass, heroXMark } from '@ng-icons/heroicons/outline';
+
 @Component({
   selector: 'app-button-icon',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({ heroMagnifyingGlass, heroXMark })],
   templateUrl: './button-icon.component.html',
   styleUrl: './button-icon.component.css'
 })
