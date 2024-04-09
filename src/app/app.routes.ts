2343ddb1688a5f86e7d2fb50f95f48ddc5cc7022
Nathalia <mesquita.natth@gmail.com>
2024-04-09 12:01:05 -0300

    added notfound page and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { MovieCatalogHomePageComponent } from './features/movie-catalog/pages/movie-catalog-home-page/movie-catalog-home-page.component';
 import { WishListComponent } from './features/wish-list/pages/wish-list.component';
 import { RankingComponent } from './features/ranking/pages/ranking/ranking.component';
+import { PageNotFoundComponent } from './features/not-found/pages/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     { path: '',   redirectTo: '/catalog', pathMatch: 'full' },
     { path: 'catalog', component: MovieCatalogHomePageComponent },
     { path: 'wish-list', component: WishListComponent },
-    { path: 'ranking', component: RankingComponent }
+    { path: 'ranking', component: RankingComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
