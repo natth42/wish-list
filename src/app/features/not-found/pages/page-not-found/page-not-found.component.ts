d4ab1086cadfa2ef69029724bef39d64f192caf2
Nathalia <mesquita.natth@gmail.com>
2024-04-10 21:56:59 -0300

    adjust not found page

--- src/app/features/not-found/pages/page-not-found/page-not-found.component.ts
+++ src/app/features/not-found/pages/page-not-found/page-not-found.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { HeaderComponent } from "@core/components/header/header.component";
 import { SideMenuComponent } from "@core/components/side-menu/side-menu.component";
+import { ButtonComponent } from "@shared/components/button/button.component";
 
 @Component({
     selector: 'app-page-not-found',
     standalone: true,
     templateUrl: './page-not-found.component.html',
     styleUrl: './page-not-found.component.css',
-    imports: [HeaderComponent, SideMenuComponent]
+    imports: [HeaderComponent, SideMenuComponent, ButtonComponent]
 })
 export class PageNotFoundComponent {
+    constructor(private router: Router) {}
 
+    goToCatalog() {
+        this.router.navigate(['/catalog']);
+    }
 }
